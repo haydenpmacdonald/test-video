a472033e3dd4ee2e7deb585706638292a7f0b9b0
Hayden MacDonald <haydenpmacdonald@gmail.com>
2021-05-11 16:10:25 -0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-// import CodingVideo from 'video/keyboard.mp4'
-import footage from './footage'
+
 import video from "./keyboard.mov";
-import ReactPlayer from 'react-player'
+
 
 function App() {
   return (
@@ -10,11 +9,6 @@ function App() {
       <video controls autoPlay loop muted>
         <source src={video} type="video/mp4" />
       </video>
-      {/* {footage.map(foot => (
-        <video controls autoPlay loop muted src={foot.video_name} preload="preload" className='video-test' allowfullscreen loop='3' title='test' allow="autoplay" muted autoPlay={true} frameborder="0" alt='test' >
-          <source src={foot.video_name} type="video/mp4"></source>
-        </video>
-      ))} */}
     </div>
   );
 }
