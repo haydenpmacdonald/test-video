b542a615be002e8e57954374e464cbe2270aab25
Hayden MacDonald <haydenpmacdonald@gmail.com>
2021-05-11 15:11:08 -0700

    non automatic play

--- src/App.js
+++ src/App.js
@@ -5,25 +5,14 @@ function App() {
   return (
     <div className="App">
       
-        <div className='overlay'>
+        {/* <div className='overlay'>
           <h1>title</h1>
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </div>
-        <video className='video-test'
-        autoPlay loop muted
-        style={{
-          position: 'absolute',
-          width: '100%',
-          heght: '100%',
-          left: '50%',
-          top: '40%',
-          objectFit: 'cover',
-          transform: 'translate(-50%, -80%)',
-        }}
-      >
+        </div> */}
+        <iframe allowfullscreen loop='3' title='test' height='500px' width='100%' src='video/keyboard.mp4' allow="autoplay" frameborder="0" alt='test'>
         
-        <source src='video/keyboard.mp4' />
-      </video>
+    
+      </iframe>
     </div>
   );
 }
